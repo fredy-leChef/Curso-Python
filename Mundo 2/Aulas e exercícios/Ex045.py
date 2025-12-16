adb156eff1687952d292103e2ea5e30676ef48da
Frederico Figueira <ffigueira.bio@gmail.com>
2025-12-16 16:52:46 -0300

    Update Ex045.py

--- Mundo 2/Aulas e exercícios/Ex045.py
+++ Mundo 2/Aulas e exercícios/Ex045.py
@@ -1,23 +1,29 @@
 #Crie um programa que faça o computador jogar jookenpô com você.
 import random
 from time import sleep
-print('=-=' * 20)
+print('=-=' * 10)
 print('Vamos jogar Jokenpô!')
-print('=-=' * 20)
+print('=-=' * 10)
 
 itens = ('Pedra','Papel','Tesoura')
-jo = random.randint(0, 2) #Pc pensando em qual irá escolher
-pc = itens[jo] #Mudando a escolha do pc para as possíveis opções.
 
-user = int(input('Escolha sua jogada:\n[0] Pedra\n[1] Papel\n[2] Tesoura\nQual é a sua escolha? '))
-if user > 2:
-    print('tente novamente.')
+computador = random.randint(0, 2) #Pc pensando em qual irá escolher
+pc = itens[computador] #Mudando a escolha do pc para as possíveis opções.
 
-usuario = lista[user] #mudando o número escolhido pela jogada.
+user = int(input('Escolha sua jogada:\n[ 0 ] Pedra\n[ 1 ] Papel\n[ 2 ] Tesoura\nQual é a sua escolha? '))
 
+usuario = itens[user] #mudando o número escolhido pela jogada.
+print('=-=' * 10)
 print(f'Você escolheu {usuario} e eu escolhi {pc}')
+print('=-=' * 10)
 
-if user == 0 and jo == 2:
+
+
+
+
+
+
+'''if user == 0 and jo == 2:
     print(f'{usuario} ganha de {pc}. VOCÊ VENCEU!')
 elif user == 1 and jo == 0:
     print(f'{usuario} ganha de {pc}. VOCÊ VENCEU!')
@@ -30,4 +36,4 @@ elif user == 1 and jo == 2:
 elif user == 2 and jo == 0:
     print(f'{pc} ganha de {usuario}, EU GANHEI!')
 else:
-    print('EMPATE')
+    print('EMPATE')'''
